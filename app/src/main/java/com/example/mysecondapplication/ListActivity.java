b427039a89245adf717e75821a39da14bea94f1e
Gaveen Aperano Mouloungui <gaveen@MacBook-Pro-de-Gaveen.local>
2022-06-09 10:16:13 +0200

    save  data

--- app/src/main/java/com/example/mysecondapplication/ListActivity.java
+++ app/src/main/java/com/example/mysecondapplication/ListActivity.java
@@ -3,6 +3,7 @@ package com.example.mysecondapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,6 +14,10 @@ public class ListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+
+        ListView lv = findViewById(R.id.lv_etudiant);
+        EtudiantAdapter adap = new EtudiantAdapter(getDataSource(),ListActivity.this);
+        lv.setAdapter(adap);
     }
 
     public List<Etudiant> getDataSource(){
